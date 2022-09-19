c2a045d657bf4c4d99264fa773510286ef94b903
entest <hai@entest.io>
2022-09-19 05:48:20 +0000

    first

---
+++ lambda/index.py
@@ -0,0 +1,27 @@
+import datetime
+import time
+import json
+
+def lambda_handler(event, context) -> json:
+    """
+    simple lambda function
+    """
+
+    # time stamp
+    now = datetime.datetime.now()
+    time_stamp = now.strftime("%Y/%m/%d %H:%M:%S.%f")
+
+    # sleep
+    time.sleep(2)
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Methods": "OPTIONS,GET"
+        },
+        'body': json.dumps({
+            'message': f'lambda {time_stamp} {event}'
+        })
+    }
--- lib/apigw-lambda-basic-stack.ts
+++ lib/apigw-lambda-basic-stack.ts
@@ -1,16 +1,36 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Duration } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as path from "path";
+import * as fs from "fs";
 
 export class ApigwLambdaBasicStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // lambda function
+    const func = new cdk.aws_lambda.Function(this, "SimpleLambda", {
+      functionName: "SimpleLambda",
+      code: cdk.aws_lambda.Code.fromInline(
+        fs.readFileSync(path.resolve(__dirname, "./../lambda/index.py"), {
+          encoding: "utf-8",
+        })
+      ),
+      runtime: cdk.aws_lambda.Runtime.PYTHON_3_8,
+      memorySize: 512,
+      timeout: Duration.seconds(10),
+      handler: "index.handler",
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ApigwLambdaBasicQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // apigatway
+    const apigw = new cdk.aws_apigateway.RestApi(this, "ApiGwDemo", {
+      restApiName: "ApiGwDemo",
+    });
+
+    // resource
+    const resource = apigw.root.addResource("order");
+
+    // method and lambda integration
+    resource.addMethod("GET", new cdk.aws_apigateway.LambdaIntegration(func));
   }
 }
